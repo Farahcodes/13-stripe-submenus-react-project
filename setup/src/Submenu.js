6a93248e59b2b24c7a2cac168d2159f5ec5113c0
farah <farah.fournat@gmail.com>
2022-01-26 14:11:28 +0100

    dynamically changing the submenu position

--- setup/src/Submenu.js
+++ setup/src/Submenu.js
@@ -3,9 +3,17 @@ import { useGlobalContext } from './context'
 
 
 const Submenu = () => {
-  const{isSubmenuOpen}= useGlobalContext()
+  const{isSubmenuOpen, location}= useGlobalContext()
+  const container = useRef(null)
+  useEffect(()=>{
+    const submenu = container.current;
+    const{center,bottom} = location;
+    submenu.style.left = `${center}px`
+    submenu.style.top = `${bottom}px`
+
+  },[location])
   return (
-    <aside className={`${isSubmenuOpen?'submenu show':'submenu'}`}>
+    <aside className={`${isSubmenuOpen?'submenu show':'submenu'}`} ref={container}>
       submenu
     </aside>
 
